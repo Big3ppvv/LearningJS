9751ecdbb86c32085df4dfe6c5820dced5a0dafd
Celina <joaquinruizperez2002@gmail.com>
2023-09-06 08:48:55 -0300

    Push new JS exercise

---
+++ exercise1.js
@@ -0,0 +1,20 @@
+/* Define a function that takes an array and a number like arguments.
+Add the number in the end of the array and next delete the first element.
+The function must return  the element that was delete */
+
+function return_element(array, num) {
+    array.push(num)
+    first_element = array.shift()
+    return first_element
+
+}
+
+
+
+new_array = [5,6,7,8]
+new_num = 9
+say_number = return_element(new_array, new_num)
+
+
+
+
