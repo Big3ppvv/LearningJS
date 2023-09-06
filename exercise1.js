7d6af83922bf7d32fafcb0075472d10badec90eb
Celina <joaquinruizperez2002@gmail.com>
2023-09-06 09:12:43 -0300

    Change the code correctly

--- exercise1.js
+++ exercise1.js
@@ -3,17 +3,21 @@ Add the number in the end of the array and next delete the first element.
 The function must return  the element that was delete */
 
 function return_element(array, num) {
-    array.push(num)
-    first_element = array.shift()
-    return first_element
+    array.push(num);
+    first_element = array.shift();
+    return first_element;
 
 }
 
 
 
-new_array = [5,6,7,8]
-new_num = 9
-say_number = return_element(new_array, new_num)
+var new_array = [5,6,7,8];
+var new_num = 9;
+var say_number = return_element(new_array, new_num);
+
+
+
+
 
 
 
