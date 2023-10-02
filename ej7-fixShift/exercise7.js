b24a2c8d1658e925f4e1d3c8cb03b60e8bdf6ca6
Big3ppvv <joaquinruizperez2002@gmail.com>
2023-10-02 19:47:01 -0300

    Add empty exercise. Not done yet

--- ej7-fixShift/exercise7.js
+++ ej7-fixShift/exercise7.js
@@ -13,17 +13,8 @@ const newArray = {
     // ver el largo del array 0 
     if (newArray.length === 0) return;
     // Eliminar el primer elem
-    let objectElements = newArray.elements;
-    let objectIndex = newArray.lengths;
     const deletedElements = [];
-    for(let i = 0; i <= objectIndex + 1; i++){
-      if(i === 0){
-        deletedElements.push(objectElements[i]);
-        objectElements[i] = objectElements[i + 1]
-        objectIndex--
-      }
-    }
-    console.log(objectElements)
+    
   }
 
   shift(newArray);
