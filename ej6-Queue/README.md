646eef4c6a586ec4a409c9cfd02552a232cc9c9d
Big3ppvv <joaquinruizperez2002@gmail.com>
2023-10-03 17:09:45 -0300

    Not done yet

--- ej6-Queue/README.md
+++ ej6-Queue/README.md
@@ -1,6 +1,5 @@
-# Exercise 3
+# Exercise 6
 
 ## Problem
 
-It is necessary to create a function that takes a parameter "card" and depends of the value increase or decrease the global  variable "count" and finally return a specific string with the modified value of count 
-
+It is necessary to create a function that takes two parameters "array" and "num" and add the number variable to the end of the array varibale and delete the first element without using a method
--- ej6-Queue/exercise6.js
+++ ej6-Queue/exercise6.js
@@ -1,24 +1,15 @@
 function making_queueWithLoop(array, num){
-    for(let i = 0; i <= array.length ; i++){
+    const deleted_value = [];
+    for(let i = 0; i <= array.length; i++){
         if(i === 0){
-            array.splice(0,1);
-        } 
-    }
-    array[array.length] = num; 
-    return array;
-}
-
-/*function making_queueWithEach(array, num){
-    array.array.forEach(element => {
+            array.push(num);
+        }
         
-    });
-}*/
-
+    }
+       
+}
+   
 const array = [5,6,7];
 const num = 9;
-const queue_with_ForLoop = making_queueWithLoop(array, num);
-console.log(queue_with_ForLoop)
-//const queue_with_EachFor =  making_queueWithEach(array, num);
-
-
-
+const result = making_queueWithLoop(array, num);
+console.log(result);
