a1330810a3dc5177c1bcc5d92dac62d8df2e4711
Big3ppvv <joaquinruizperez2002@gmail.com>
2023-10-07 12:26:24 -0300

    Not done yet

--- ej6-Queue/exercise6.js
+++ ej6-Queue/exercise6.js
@@ -1,18 +1,13 @@
-function making_queueWithLoop(array, num){
-    const copy = [];
-    const deleted_value = [];
-    for(let i = 0; i < array.length; i++){
-        if(i === 0){
-            deleted_value.push(array[i]) 
-        }else{
-            copy.push(array[i])
-        }   
-    }
-    copy.push(num)
-    return copy
+function makeQueueWithLoop(elements, num){
+    if(elements.length === 0) return [num];
+    const proccesedQueue = [];
+    elements.forEach( (element, index) => {
+        if(index === 0) return
+        
+    });
 }
    
 const array = [5,6,7];
 const num = 9;
-const result = making_queueWithLoop(array, num);
+const result = makeQueueWithLoop(array, num);
 console.log(result);
