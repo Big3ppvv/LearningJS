04880e43abaca3ddc95ce7b09472e5045f829185
Big3ppvv <joaquinruizperez2002@gmail.com>
2023-10-11 08:17:55 -0300

    Resolve exercise 6

--- ej6-Queue/exercise6.js
+++ ej6-Queue/exercise6.js
@@ -1,11 +1,17 @@
 function makeQueueWithLoop(elements, num){
     if(elements.length === 0) return [num];
-    const proccesedQueue = [];
-    elements.forEach( (element, index) => {
-        if(index === 0) return
-        
-    });
-}
+    const first_element = [];
+    const newQueue = [];
+    for(let i = 0; i < array.length; i++){
+        if(i === 0){
+            first_element.push(elements[i]);
+        }else{
+            newQueue.push(elements[i]);
+        }
+    }
+    newQueue.push(num);
+    return newQueue;
+}   
    
 const array = [5,6,7];
 const num = 9;
