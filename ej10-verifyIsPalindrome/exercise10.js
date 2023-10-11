a4b471da85a6f549a2521e02f8370538c541ad2c
Big3ppvv <joaquinruizperez2002@gmail.com>
2023-10-11 08:09:04 -0300

    Add new exercise

---
+++ ej10-verifyIsPalindrome/exercise10.js
@@ -0,0 +1,17 @@
+//Palíndromos son palabras o frases que al leerse de izquierda a 
+//derecha y viceversa expresan o tienen el mismo sentido.
+
+function isPalindrome(number){
+    const palindromeNumber = []
+    const result = number.string.split('')
+    palindromeNumber.push(result)
+    console.log(palindromeNumber) 
+    
+    
+    /*if(palindromeNumber === StringNumber){
+        return ;
+    }*/
+}
+
+const number = 154;
+isPalindrome(number);
