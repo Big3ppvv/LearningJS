d58ec0b2c8c27cdf96cc84a1c200cf0d1f851ec6
Big3ppvv <joaquinruizperez2002@gmail.com>
2023-10-14 10:28:01 -0300

    WIP

--- ej10-verifyIsPalindrome/exercise10.js
+++ ej10-verifyIsPalindrome/exercise10.js
@@ -2,15 +2,28 @@
 //derecha y viceversa expresan o tienen el mismo sentido.
 
 function isPalindrome(number){
-    const palindromeNumber = []
-    const result = number.string.split('')
-    palindromeNumber.push(result)
-    console.log(palindromeNumber) 
-    
-    
-    /*if(palindromeNumber === StringNumber){
-        return ;
-    }*/
+  numberOfDigits = Math.floor(Math.log10(number) + 1);
+  switch(numberOfDigits){
+    case 2:
+        var divisionNumber = 10;
+        return divisionNumber;
+    case 3:
+        var divisionNumber = 100;
+        return divisionNumber;
+    case 4:
+        var divisionNumber = 1000;
+        return divisionNumber;
+    case 5:
+        var divisionNumber = 10000;
+        return divisionNumber;
+    case 6:
+        var divisionNumber = 100000;
+        return divisionNumber;
+    case 7:
+        var divisionNumber = 1000000;
+        return divisionNumber;
+  }
+
 }
 
 const number = 154;
