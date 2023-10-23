4ceb00877cf1571fd79c7f49e5e2c8d09954cbdb
Celina <joaquinruizperez2002@gmail.com>
2023-10-23 11:14:16 -0300

    Add new exercise

--- ej10-verifyIsPalindrome/exercise10.js
+++ ej10-verifyIsPalindrome/exercise10.js
@@ -1,22 +1,23 @@
-//Palíndromos son palabras o frases que al leerse de izquierda a 
-//derecha y viceversa expresan o tienen el mismo sentido.
-
 function isPalindrome(number){
-  while(number !== 0){
-    let numberOfDigits = Math.floor(Math.log10(number) + 1);
-    let divisorNumber = Math.pow(10, numberOfDigits - 1);
-    let decreaseNumber = Math.floor(number / divisorNumber);
-    number = number - decreaseNumber * divisorNumber;
-    if(number % decreaseNumber === 0){
-      return "Is Palidnrome";
-      
-    }else{
-      isPalindrome(number);
-    }
+  //Evaluar si es negativo
 
+  if(number < 0){
+    return false
+  }
+  //Evaluar la cantidad de digitos que tiene
+  let numberOfDigits = Math.floor(Math.log10(number) + 1);
+  /* Elevar el 10 segun la cantidad de digitos para dividir el numero 
+  y disminuirlo poco a poco */
+  let divisorNumber = Math.pow(10, numberOfDigits - 1);
+  let numbersOneByOne = Math.floor(number / divisorNumber);
+  let decreasedNumber = number - numbersOneByOne * divisorNumber;
+
+  //Iterar sobre la cantidad de digitos
+
+  for(let i = 0; i <= numberOfDigits; i++){
+    isPalindrome(decreasedNumber);
   }
 }
-
-const number = 9882;
+const number = 342;
 const verifyPalindrome = isPalindrome(number);
 console.log(verifyPalindrome);
