e1fa0c6462c5dae2c67ef5cb0c24b2b575536a2f
Celina <joaquinruizperez2002@gmail.com>
2023-10-23 18:30:12 -0300

    Add new exercise

--- ej10-verifyIsPalindrome/exercise10.js
+++ ej10-verifyIsPalindrome/exercise10.js
@@ -1,23 +1,26 @@
 function isPalindrome(number){
-  //Evaluar si es negativo
-
   if(number < 0){
     return false
   }
   //Evaluar la cantidad de digitos que tiene
-  let numberOfDigits = Math.floor(Math.log10(number) + 1);
-  /* Elevar el 10 segun la cantidad de digitos para dividir el numero 
-  y disminuirlo poco a poco */
-  let divisorNumber = Math.pow(10, numberOfDigits - 1);
-  let numbersOneByOne = Math.floor(number / divisorNumber);
-  let decreasedNumber = number - numbersOneByOne * divisorNumber;
+let numberOfDigits = Math.floor(Math.log10(number) + 1);
 
   //Iterar sobre la cantidad de digitos
-
-  for(let i = 0; i <= numberOfDigits; i++){
-    isPalindrome(decreasedNumber);
+  let repetedLoop = 0 ;
+  let decreasedNumber
+  while(repetedLoop < numberOfDigits ){
+    /* Elevar el 10 segun la cantidad de digitos para dividir el numero 
+    y disminuirlo poco a poco */
+    let divisorNumber = Math.pow(10, numberOfDigits - 1);
+    let numbersOneByOne = Math.floor(number / divisorNumber);
+    decreasedNumber = number - numbersOneByOne * divisorNumber;
+    numberOfDigits--
+  }
+  if(decreasedNumber === 0){
+    return true
   }
 }
-const number = 342;
+
+const number = 898;
 const verifyPalindrome = isPalindrome(number);
 console.log(verifyPalindrome);
