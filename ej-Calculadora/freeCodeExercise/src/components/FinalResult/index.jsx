a93411056ac51edb38b98d7a0128c5449c6f9377
Big3ppv <joaquinruizperez2002@gmail.com>
2024-01-30 11:19:23 -0300

    Resolve some issues and upgrade the code

--- ej-Calculadora/freeCodeExercise/src/components/FinalResult/index.jsx
+++ ej-Calculadora/freeCodeExercise/src/components/FinalResult/index.jsx
@@ -1,16 +1,16 @@
-const FinalResult = ({ insertNumbersSimbols, numbersAndSimbols }) => {
+const FinalResult = ({ onInsert, numbersAndSimbols }) => {
 
-  const giveResult = (insertNumbersSimbols, numbersAndSimbols) => {
+  const giveResult = (onInsert, numbersAndSimbols) => {
     try {
       const input = [...numbersAndSimbols];
       const completeInput = [(input.join(""))];
       const evaluation = completeInput.map((value) =>{
       return eval(value)
       })
-      insertNumbersSimbols(evaluation);
+      onInsert(evaluation);
       
     } catch (SyntaxError) {
-      insertNumbersSimbols('Put a correct number')
+      onInsert('Put a correct number')
       
     }
     
@@ -20,7 +20,7 @@ const FinalResult = ({ insertNumbersSimbols, numbersAndSimbols }) => {
 
   return (
     <div>
-      <p onClick={() => giveResult(insertNumbersSimbols, numbersAndSimbols)} className=" cursor-pointer hover:bg-sky-700 bg-slate-900 rounded-lg w-16 h-16 text-white text-xl text-center p-4">=</p>
+      <p onClick={() => giveResult(onInsert, numbersAndSimbols)} className=" cursor-pointer hover:bg-sky-700 bg-slate-900 rounded-lg w-16 h-16 text-white text-xl text-center p-4">=</p>
     </div>
   )
 }
