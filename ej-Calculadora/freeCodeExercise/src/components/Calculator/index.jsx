e78b8d93678ac9e9029227e7f68134f1b8f02b2e
Big3ppv <joaquinruizperez2002@gmail.com>
2024-01-30 10:52:08 -0300

    Change issues

--- ej-Calculadora/freeCodeExercise/src/components/Calculator/index.jsx
+++ ej-Calculadora/freeCodeExercise/src/components/Calculator/index.jsx
@@ -1,19 +1,19 @@
 import { useState, useEffect } from "react";
-import NumbersAndSimbols from "../NumbersAndSimbols";
+import InsertNumbersSimbols from "../InsertNumbersSimbols";
 const Calculator = () => {
-  const [numberSimbol, setNumberSimbol] = useState('');
+  const [numberSymbol, setNumberSymbol] = useState('');
 
   useEffect(() => {
     const input = document.getElementById('inside-numbers');
-    input.value = numberSimbol;
+    input.value = numberSymbol;
 
-  },[numberSimbol])
+  },[numberSymbol])
 
   return (
     <div>
       <div className="grid bg-slate-300 rounded-lg w-1/3 h-screen border-white border-2 m-auto mt-32">
         <input className="w-10/12 bg-slate-900 ml-10 mt-8 h-20 rounded-full text-white pl-8 text-2xl" type="text" id="inside-numbers" />
-        <NumbersAndSimbols insertNumbersSimbols={setNumberSimbol} numbersAndSimbols={numberSimbol} />
+        < InsertNumbersSimbols onInsert={setNumberSymbol} numbersAndSimbols={numberSymbol} />
       </div>
     </div>
   )
---
+++ ej-Calculadora/freeCodeExercise/src/components/InsertNumbersSimbols/index.jsx
@@ -0,0 +1,69 @@
+import ClearComponent from "../ClearInput";
+import FinalResult from "../FinalResult";
+
+const InsertNumbersSimbols = ({ onInsert, numbersAndSimbols }) => {
+  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const clickedNumbers = [];
+  const clickedSimbols = [];
+
+
+  const addValues = (onInsert, numbersAndSimbols, value) => {
+    const paragraphContent = value.currentTarget.textContent;
+    let total = '';
+    
+    paragraphContent in numbers ? clickedNumbers.push(paragraphContent)
+      : clickedSimbols.push(paragraphContent)
+
+    clickedNumbers.map((numbers) => {
+      total += numbers
+      onInsert(numbersAndSimbols + total)
+      
+    })
+
+    clickedSimbols.map((simbols) => {
+      total += simbols
+      onInsert(numbersAndSimbols + total)
+    })
+    
+  }
+
+  return (
+    <div className="inline-grid m-auto gap-8">
+
+      <section className="inline-grid grid-cols-4 place-items-center mt-8 gap-8">
+        <p onClick={(value) => addValues(onInsert, numbersAndSimbols, value)} className="cursor-pointer hover:bg-sky-700 bg-slate-900 rounded-lg w-16 h-16 text-white text-xl text-center p-4">1</p>
+        <p onClick={(value) => addValues(onInsert, numbersAndSimbols, value)} className="cursor-pointer hover:bg-sky-700 bg-slate-900 rounded-lg w-16 h-16 text-white text-2xl text-center p-4">2</p>
+        <p onClick={(value) => addValues(onInsert, numbersAndSimbols, value)} className="cursor-pointer hover:bg-sky-700 bg-slate-900 rounded-lg w-16 h-16 text-white text-2xl text-center p-4">3</p>
+        <p onClick={(value) => addValues(onInsert, numbersAndSimbols, value)} className=" cursor-pointer hover:bg-sky-700 bg-green-500 rounded-lg w-16 h-16 text-white text-2xl text-center p-4">+</p>
+      </section>
+
+      <section className="inline-grid  grid-cols-4 place-items-center gap-8">
+        <p onClick={(value) => addValues(onInsert, numbersAndSimbols, value)} className="cursor-pointer hover:bg-sky-700 bg-slate-900 rounded-lg w-16 h-16 text-white text-2xl text-center p-4">4</p>
+        <p onClick={(value) => addValues(onInsert, numbersAndSimbols, value)} className="cursor-pointer hover:bg-sky-700 bg-slate-900 rounded-lg w-16 h-16 text-white text-2xl text-center p-4">5</p>
+        <p onClick={(value) => addValues(onInsert, numbersAndSimbols, value)} className="cursor-pointer hover:bg-sky-700 bg-slate-900 rounded-lg w-16 h-16 text-white text-2xl text-center p-4">6</p>
+        <p onClick={(value) => addValues(onInsert, numbersAndSimbols, value)} className="cursor-pointer hover:bg-sky-700 bg-green-500 rounded-lg w-16 h-16 text-white text-2xl text-center p-4">-</p>
+      </section>
+
+      <section className="inline-grid grid-cols-4 place-items-center gap-8">
+        <p onClick={(value) => addValues(onInsert, numbersAndSimbols, value)} className="cursor-pointer hover:bg-sky-700 bg-slate-900 rounded-lg w-16 h-16 text-white text-2xl text-center p-4">7</p>
+        <p onClick={(value) => addValues(onInsert, numbersAndSimbols, value)} className="cursor-pointer hover:bg-sky-700 bg-slate-900 rounded-lg w-16 h-16 text-white text-2xl text-center p-4">8</p>
+        <p onClick={(value) => addValues(onInsert, numbersAndSimbols, value)} className="cursor-pointer hover:bg-sky-700 bg-slate-900 rounded-lg w-16 h-16 text-white text-2xl text-center p-4">9</p>
+        <p onClick={(value) => addValues(onInsert, numbersAndSimbols, value)} className="cursor-pointer hover:bg-sky-700 bg-green-500 rounded-lg w-16 h-16 text-white text-2xl text-center p-4">*</p>
+      </section>
+
+      <section className="inline-grid grid-cols-4 place-items-center gap-8">
+        <FinalResult onInsert={onInsert} numbersAndSimbols={numbersAndSimbols}/>
+        <p onClick={(value) => addValues(onInsert, numbersAndSimbols, value)} className="cursor-pointer hover:bg-sky-700 bg-slate-900 rounded-lg w-16 h-16 text-white text-2xl text-center p-4">0</p>
+        <p onClick={(value) => addValues(onInsert, numbersAndSimbols, value)} className="cursor-pointer hover:bg-sky-700 bg-slate-900 rounded-lg w-16 h-16 text-white text-2xl text-center p-4">.</p>
+        <p onClick={(value) => addValues(onInsert, numbersAndSimbols, value)} className="cursor-pointer hover:bg-sky-700 bg-green-500 rounded-lg w-16 h-16 text-white text-2xl text-center p-4">/</p>
+      </section>
+
+      <section>
+        <ClearComponent clearComponent={onInsert} />
+      </section>
+
+    </div>
+  )
+}
+
+export default InsertNumbersSimbols;
